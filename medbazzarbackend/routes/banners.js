cdb3e33aea771408481e7b91727ebdba0cb9a5e9
mukul-sharma085 <163444011+mukul-sharma085@users.noreply.github.com>
2024-03-26 22:34:20 +0530

    Update banners.js

--- medbazzarbackend/routes/banners.js
+++ medbazzarbackend/routes/banners.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router  = express.Router()
 var pool = require('./pool')
+
 var upload = require('./multer')
 
 router.post('/submit_banners',upload.any(),function(req,res,next){
